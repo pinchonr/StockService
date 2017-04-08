7ef6c2eeb2dcdb36b73b1c677e7023ef93c2b577
Robin <robin.pinchon@gmail.com>
2017-04-08 10:34:27 +0200

    adding values between simple cotes

--- src/main/java/com/example/Stock.java
+++ src/main/java/com/example/Stock.java
@@ -55,7 +55,7 @@ public class Stock {
 			 connection = getConnection();
 			Statement stmt = connection.createStatement();
 			stmt.execute("CREATE TABLE IF NOT EXISTS LIBRARY(ID INT PRIMARY KEY NOT NULL, ISBN TEXT NOT NULL, TITLE TEXT NOT NULL, AUTHOR TEXT NOT NULL, STOCK INT NOT NULL);");
-			stmt.executeUpdate("INSERT INTO LIBRARY (ISBN,TITLE,AUTHOR,STOCK) VALUES (1234567890111,TEST,TEST,20)");
+			stmt.executeUpdate("INSERT INTO LIBRARY (ISBN,TITLE,AUTHOR,STOCK) VALUES ('1234567890111','TEST','TEST','20')");
 			return "Table exists";
 		} catch (Exception e) {
 			return e.getMessage();
